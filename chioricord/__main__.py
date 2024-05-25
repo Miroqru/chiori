38128aecbfb5441c4a75a7edffe6eb7669ddc92c
Milinuri Nirvalen <pentergust@mail.ru>
2024-05-25 22:20:00 +0400

    Add simple gpt command

--- chioricord/__main__.py
+++ chioricord/__main__.py
@@ -1,5 +1,5 @@
 import asyncio
-from pytkb5.bot import start_bot
+from chioricord.bot import start_bot
 
 
 if __name__ == "__main__":
--- chioricord/bot.py
+++ chioricord/bot.py
@@ -8,13 +8,17 @@ from discord.ext import commands
 
 from loguru import logger
 
-from pytkb5 import config
+from chioricord import config
+from g4f.client import Client
+import g4f.Provider.Aichatos
+
 
 
 bot = commands.Bot(
     command_prefix=commands.when_mentioned_or("c!"),
     intents=Intents.all(),
 )
+gpt_client = Client()
 
 
 @bot.event
@@ -24,6 +28,21 @@ async def on_ready():
 @bot.command(description="Responds with 'World'")
 async def hello(ctx: commands.Context):
     await ctx.send("Hello!")
+
+@bot.command(description="Talk to ChatGPT")
+async def gpt(ctx: commands.context, *, args: str | None):
+    if args is None:
+        return await ctx.send("Использовать c!gpt <запрос>.")
+
+    response = gpt_client.chat.completions.create(
+        model="gpt-4",
+        messages=[
+            {"role": "system", "content":"Твоя задача будет подробно отечать на русском языке"},
+            {"role": "system", "content": args}
+        ],
+        provider=g4f.Provider.Aichatos
+    )
+    await ctx.send(response.choices[0].message.content)
 
 async def start_bot():
     logging.basicConfig(level=logging.INFO)
