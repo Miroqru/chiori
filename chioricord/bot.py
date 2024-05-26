09807c1233588087e37f75daf044289529c49c2c
Milinuri Nirvalen <pentergust@mail.ru>
2024-05-26 22:23:40 +0400

    Cogs: Move GPT cog

--- chioricord/bot.py
+++ chioricord/bot.py
@@ -9,16 +9,20 @@ from discord.ext import commands
 from loguru import logger
 
 from chioricord import config
-from g4f.client import Client
-import g4f.Provider.Aichatos
 
+
+# Глобальные переменные
+# =====================
 
 bot = commands.Bot(
     command_prefix=commands.when_mentioned_or(config.BOT_PREFIX),
     intents=Intents.all(),
 )
-gpt_client = Client()
 COGS_PATH = Path("cogs/")
+
+
+# Обработка событий
+# =================
 
 @bot.event
 async def on_ready():
@@ -33,24 +37,16 @@ async def on_ready():
     )
 
 
+# Обработка команд
+# ================
+
 @bot.command(description="Responds with 'World'")
 async def hello(ctx: commands.Context):
-    await ctx.send("Hello!")
+    await ctx.send("World!")
 
-@bot.command(description="Talk to ChatGPT")
-async def gpt(ctx: commands.context, *, args: str | None):
-    if args is None:
-        return await ctx.send("Использовать c!gpt <запрос>.")
 
-    response = gpt_client.chat.completions.create(
-        model="gpt-4",
-        messages=[
-            {"role": "system", "content":"Твоя задача будет подробно отечать на русском языке"},
-            {"role": "system", "content": args}
-        ],
-        provider=g4f.Provider.Aichatos
-    )
-    await ctx.send(response.choices[0].message.content)
+# Запуск бота
+# ===========
 
 async def start_bot():
     logging.basicConfig(level=logging.INFO)
@@ -63,4 +59,3 @@ async def start_bot():
             logger.info("Loaded cog: {}", p)
 
     await bot.start(config.BOT_TOKEN)
-
