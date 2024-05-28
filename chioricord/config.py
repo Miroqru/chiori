4fecec4beca3faf4aabef32dcd7e9a12404a8176
Milinuri Nirvalen <pentergust@mail.ru>
2024-05-28 20:36:43 +0400

    Bot: Add BOT_OWNER config param

--- chioricord/config.py
+++ chioricord/config.py
@@ -6,6 +6,7 @@ Author: Milinuri Nirvalen
 from os import getenv
 
 from dotenv import load_dotenv
+from loguru import logger
 
 load_dotenv()
 BOT_TOKEN = getenv("BOT_TOKEN")
@@ -14,3 +15,9 @@ if BOT_TOKEN is None:
     raise TypeError("You need to set discord bot token")
 
 BOT_PREFIX = getenv("BOT_PREFIX", "c!")
+BOT_OWNER = getenv("BOT_OWNER")
+
+if BOT_OWNER is None:
+    logger.warning("Bot owner id is empty.")
+    logger.warning("No one will be able to control the bot system")
+
