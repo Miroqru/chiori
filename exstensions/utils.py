3d57d0510bbbfabf737c54a7e5634f17dad32948
Milinuri Nirvalen <pentergust@mail.ru>
2024-08-12 09:39:51 +0400

    Ext:Utils: Add user info command

--- exstensions/utils.py
+++ exstensions/utils.py
@@ -5,11 +5,14 @@
 Предоставляет
 -------------
 
-- /avatar [user] - Получает аватар участника
+- /delmsg [count] - Удаляет сообщения из канала.
+- /user [user] - Информация о пользователе.
 
-Version: v0.1
+Version: v0.2 (12)
 Author: Milinuri Nirvalen
 """
+
+from datetime import date, timedelta
 
 import arc
 import hikari
@@ -21,26 +24,101 @@ plugin = arc.GatewayPlugin("utils")
 # ==================
 
 @plugin.include
-@arc.slash_command("avatar", description="Получить аватар пользователя.")
+@arc.slash_command(
+    "delmsg",
+    description="Удалить несколько сообщений в канале",
+    default_permissions=hikari.Permissions.MANAGE_MESSAGES
+)
+async def delmsg_handler(
+    ctx: arc.GatewayContext,
+    count: arc.Option[int, arc.IntParams("Сколько удалить сообщений (10)")] = 10
+):
+    channel = ctx.get_channel()
+    if channel is not None:
+        count = min(max(count, 1), 100)
+        ids = []
+        for i, message in enumerate(await channel.fetch_history()):
+            ids.append(message.id)
+            if i+1 == count:
+                break
+        await channel.delete_messages(ids)
+        await ctx.respond(f"Удалено {count} сообщений",
+            flags=hikari.MessageFlag.EPHEMERAL
+        )
+    else:
+        await ctx.respond(
+            "Данная команда только для текстовых каналов.",
+            flags=hikari.MessageFlag.EPHEMERAL
+        )
+
+def str_delta(delta: timedelta) -> str:
+    years, days = divmod(delta.days, 365)
+    if years > 0:
+        return f"{years} л. {days} д."
+    else:
+        return f"{delta.days} д."
+
+def get_member_info(member: hikari.Member) -> hikari.Member:
+    today = date.today()
+    create_delta = str_delta(today - member.created_at.date())
+    join_delta = str_delta(today - member.joined_at.date())
+
+    role = member.get_top_role()
+    if role is not None:
+        color = role.color
+    else:
+        color = hikari.Color(0xcc66ff)
+
+    return hikari.Embed(
+        title=f"Участник {member.global_name}",
+        description=(
+            f"**ID**: {member.id}\n"
+            f"**Ник**: {member.nickname}\n"
+            f"**Создан**: {member.created_at.strftime('%d/%m/%Y %H:%M:%S')}\n"
+            f"**Существует**: {create_delta}\n"
+            "**Присоеденился**: "
+            f"{member.joined_at.strftime('%d/%m/%Y %H:%M:%S')}\n"
+            f"**С нами**: {join_delta}"
+        ),
+        color=color
+    ).set_thumbnail(member.avatar_url)
+
+
+def get_user_info(user: hikari.User) -> hikari.Embed:
+    today = date.today()
+    create_delta = str_delta(today - user.created_at.date())
+
+    return hikari.Embed(
+        title=f"Пользователь {user.global_name}",
+        description=(
+            f"**ID**: {user.id}\n"
+            f"**Создан**: {user.created_at.strftime('%d/%m/%Y %H:%M:%S')}\n"
+            f"**Существует**: {create_delta}"
+        ),
+        color=hikari.Color(0xcc66ff)
+    ).set_thumbnail(user.avatar_url)
+
+
+@plugin.include
+@arc.slash_command("user", description="Информация о пользователе")
 async def nya_handler(
     ctx: arc.GatewayContext,
     user: arc.Option[
         hikari.User | None, arc.UserParams("Кого нужно някнуть")
     ] = None
 ) -> None:
-    """простая команда для получения аватара пользоватея.
-
-    Извлекает аватар из экземпляра пользователя.
-    Вы можете выбрать как конкретного пользователя, так и выбрать себя,
-    оставив аргумент user пустым.
-    """
     if user is None:
         user = ctx.user
+    guild = ctx.get_guild()
+    if guild is not None:
+        member = guild.get_member(user)
+        if member is not None:
+            embed = get_member_info(member)
+        else:
+            embed = get_user_info(user)
+    else:
+        embed = get_user_info(user)
 
-    embed = hikari.Embed(
-        title="О пользователе",
-        color=user.accent_color
-    ).set_image(user.avatar_url)
     await ctx.respond(embed=embed)
 
 
