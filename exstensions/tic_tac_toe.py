3cf4c43e6af9b1b13e783ec7e11a175def540fd3
Milinuri Nirvalen <pentergust@mail.ru>
2025-03-07 16:29:01 +0400

    Ext:Ttt: Update docs

--- exstensions/tic_tac_toe.py
+++ exstensions/tic_tac_toe.py
@@ -4,20 +4,20 @@
 Есть поле 3 на 3, а также два игрока, которые играют в эту игру.
 Задача каждого игрока, собрать 3 одинаковых элемента в ряд.
 не важно, по вертикали, горизонтали или диагонали.
-Выйграет первый, кто соберёт 3 крестика или нолика.
+Выигрывает первый, кто соберёт 3 крестика или нолика.
 
 Есть два варианта игры:
 
-- **Классический**: У вас есть полсе 3 на 3, а также два участника.
+- **Классический**: У вас есть после 3 на 3, а также два участника.
 - **Бесконечный**: Похож на обычный, однако со временем выши старые
-  ходы стирются и клетки освобождаются.
+  ходы стирается и клетки освобождаются.
 
 Предоставляет
 -------------
 
 - /ttt - Игра крестики-нолики.
 
-Version: v0.5.1 (19)
+Version: v0.5.2 (20)
 Author: Milinuri Nirvalen
 """
 
@@ -28,41 +28,48 @@ import miru
 # Глобальные переменные
 # =====================
 
-plugin = arc.GatewayPlugin("ttt")
+plugin = arc.GatewayPlugin("tic_tac_toe")
 
-# Как будут выглядить крестик и нолик
+# Как будут выглядеть крестик и нолик
 # Возможно в будущем появится настройки для стиля элементов
-_TTT_SIM = [
-    "⭕", "❌"
-]
+_TTT_SIM = ["⭕", "❌"]
 
-# Все выйгрышные комбинации, при которых надо собрать 3 элемента в ряд
+# Все выигрышные комбинации, при которых надо собрать 3 элемента в ряд
 _WIN_PATTERNS = [
-    (0, 1, 2), (3, 4, 5), (6, 7, 8),
-    (0, 3, 6), (1, 4, 7), (2, 5, 8),
-    (0, 4, 8), (2, 4, 6)
+    (0, 1, 2),
+    (3, 4, 5),
+    (6, 7, 8),
+    (0, 3, 6),
+    (1, 4, 7),
+    (2, 5, 8),
+    (0, 4, 8),
+    (2, 4, 6),
 ]
 
+_GAME_RULES = (
+    "- Нажмите на свободно поле, чтобы отметить его своим.\n"
+    "- Для победы соберите 3 знака по диагонали/вертикали/горизонтали.\n"
+    "- **Бесконечный режим**: Со временем клетки будут освобождаться."
+)
 
 # Вспомогательные классы представления кнопок и игрового поля
 # ===========================================================
+
 
 class GameButton(miru.Button):
     """Описывает одну из клеток поля.
 
     По умолчанию она ничем не примечательная.
-    Когда на неё нажимает игрок, она становится или кретиком или
+    Когда на неё нажимает игрок, она становится или крестиком или
     ноликом, в зависимости от игрока.
 
-    Если выбран бесконечный режим, то со времнем данная клетка
+    Если выбран бесконечный режим, то со временем данная клетка
     будет освобождена от пользователя.
     """
 
-    def __init__(self, index: int, row: int):
+    def __init__(self, index: int, row: int) -> None:
         super().__init__(
-            label="⬛",
-            style=hikari.ButtonStyle.SECONDARY,
-            row=row
+            label="⬛", style=hikari.ButtonStyle.SECONDARY, row=row
         )
         self.index = index
         self.opener: hikari.User | None = None
@@ -86,45 +93,40 @@ class GameButton(miru.Button):
         if not self.view.validate_player(ctx):
             # Удаляем сообщение через 10 секунд, чтобы не засорять чат
             return await ctx.respond(
-                self.view.no_valid_player_message(),
-                delete_after=10
+                self.view.no_valid_player_message(), delete_after=10
             )
 
         self.set_open(ctx)
 
-        # Сначала идёт проверка выйгрышных комбинаций
+        # Сначала идёт проверка выигрышных комбинаций
         # А уже после проверка на отсутствие слотов
-        # Дабы решить проблему когла вы выйгрываете последним ходом
+        # Дабы решить проблему когда вы выигрываете последним ходом
         winner = self.view.is_game_over(self.index)
         if winner is not None:
             await ctx.edit_response(
-                self.view.end_game_message(winner),
-                components=self.view
+                self.view.end_game_message(winner), components=self.view
             )
             self.view.stop()
             return
 
         if self.view.cell_left == 0:
             await ctx.edit_response(
-                self.view.end_game_no_winner(),
-                components=self.view
+                self.view.end_game_no_winner(), components=self.view
             )
             self.view.stop()
             return
 
         self.view.next_player()
         await ctx.edit_response(
-            self.view.get_game_status(),
-            components=self.view
+            self.view.get_game_status(), components=self.view
         )
-
 
     def set_open(self, ctx: miru.ViewContext) -> None:
         """помечает клетку как кем-то открытую.
 
         Она окрасится в синий цвет и станет недоступна для
         использования.
-        Также текст клетки изменится на кретик или нолик в
+        Также текст клетки изменится на крестик или нолик в
         зависимости от игрока.
         В конце отмечает кем была открыта клетка.
 
@@ -137,9 +139,8 @@ class GameButton(miru.Button):
         self.view.cell_left -= 1
         self.opener = ctx.user
 
-
     def before_close(self) -> None:
-        """подготовливает клетку к высвобождению.
+        """Подготавливает клетку к высвобождению.
 
         Окрашивает его в нейтральный цвет, а также убирает владельца.
         """
@@ -147,7 +148,7 @@ class GameButton(miru.Button):
         self.opener = None
 
     def set_close(self) -> None:
-        """Помечает клетку как готовую к высвобождени."""
+        """Помечает клетку как готовую к освобождению."""
         self.view.cell_left += 1
 
     def after_close(self) -> None:
@@ -158,17 +159,16 @@ class GameButton(miru.Button):
         self.disabled = False
         self.label = "⬛"
 
-
     def set_winner(self) -> None:
         """Помечет клетку как победную, окрашивая её в зеленый цвет."""
         self.style = hikari.ButtonStyle.SUCCESS
 
 
 class TicTacToeView(miru.View):
-    """Предсталвение игрового поля крестики-нолики.
+    """Представление игрового поля крестики-нолики.
 
     У нас есть игровое поле 3 на 3 кнопки, а также два игрока,
-    котоыре играют в эту игру.
+    которые играют в эту игру.
     Задача игроков, собрать подходящий паттерн из одинаковых символов.
     Выиграет первый у кого это получится.
 
@@ -179,7 +179,7 @@ class TicTacToeView(miru.View):
     :rtype endless: bool
     """
 
-    def __init__(self, endless: bool = False):
+    def __init__(self, endless: bool = False) -> None:
         super().__init__()
         self.endless = endless
 
@@ -194,13 +194,12 @@ class TicTacToeView(miru.View):
 
         self.start_game()
 
-
     def start_game(self) -> None:
         """Начинает новую игру крестики-нолики.
 
         Очищает старые данные, формирует игровое поле.
         """
-        # Очишаем старые данные
+        # Очищаем старые данные
         self._board.clear()
         self._players.clear()
         self.cell_left = 9
@@ -214,15 +213,15 @@ class TicTacToeView(miru.View):
             self.add_item(button)
 
     def next_player(self) -> None:
-        """Переключает курсок на следующего игрока."""
+        """Переключает курсор на следующего игрока."""
         self._cur = (self._cur + 1) % 2
 
     def validate_player(self, ctx: miru.ViewContext) -> bool:
         """Проверяет что игрок может играть в крестики-нолики.
 
         Если ещё никто не прикасался к полю, добавит первого
-        пользователя в писок игроков, кто нажмёт на кнопку.
-        Далее добавит следющего игрока в игру.
+        пользователя в список игроков, кто нажмёт на кнопку.
+        Далее добавит следующего игрока в игру.
         Теперь по очереди выбор будет между двумя добавленными
         игроками.
 
@@ -234,7 +233,7 @@ class TicTacToeView(miru.View):
         :rtype: bool
         """
         # Добавляем игроков, если их не хватает
-        if len(self._players) < 2:
+        if len(self._players) < 2:  # noqa: PLR2004
             if len(self._players) == 0:
                 self._players.append(ctx.user)
             elif ctx.user != self._players[0]:
@@ -252,8 +251,8 @@ class TicTacToeView(miru.View):
 
         Данный метод вызывается при каждом нажатии кнопки.
         Он проверяет, нет ли ни у кого совпадающих элементов.
-        Если кто-то уже осбрал комбинацию для победы, тогда мы вернём
-        экземпляр этого учатсника игры.
+        Если кто-то уже собрал комбинацию для победы, тогда мы вернём
+        экземпляр этого участника игры.
         Иначе же игра продолжается.
 
         Если выбран бесконечный режим игры, то помимо этого будет
@@ -267,7 +266,7 @@ class TicTacToeView(miru.View):
         if self.endless:
             self.register_open(index)
 
-        # Тут будет говнокод
+        # Тут будет всё очень печально
         for a_index, b_index, c_index in _WIN_PATTERNS:
             a_btn = self._board[a_index]
             b_btn = self._board[b_index]
@@ -282,7 +281,7 @@ class TicTacToeView(miru.View):
             if c_btn.opener is None:
                 continue
 
-            if (a_btn.opener == b_btn.opener and b_btn.opener == c_btn.opener):
+            if a_btn.opener == b_btn.opener and b_btn.opener == c_btn.opener:
                 a_btn.set_winner()
                 b_btn.set_winner()
                 c_btn.set_winner()
@@ -292,7 +291,7 @@ class TicTacToeView(miru.View):
     def register_open(self, index: int) -> None:
         """Регистрирует открытие всех клеток.
 
-        Тольок для бесконечного режима игры.
+        Только для бесконечного режима игры.
         Запоминает порядок при котором открывались поля в игре.
         Чтобы постепенно их "забывать".
         Этот процесс происходит в 3 этапа:
@@ -317,23 +316,19 @@ class TicTacToeView(miru.View):
             self._board[i].before_close()
             self._prepare_close = i
 
-
     # Вспомогательные методы для сборки сообщений
     # ===========================================
 
     def get_players(self) -> str:
         """Возвращает строковый список игроков.
 
-        Исползуется чтобы отобразить список всех кто играет в игру.
+        Используется чтобы отобразить список всех кто играет в игру.
         Это лучше, чем гадать, где в данный момент играет.
         Если игроков недостаточно, выводит сообщение как можно
-        присоедениться к игре.
-
-        :return: Строка со списком всех игроков.
-        :rtupe: str
+        присоединиться к игре.
         """
-        if len(self._players) < 2:
-            res = "Нажмите на поле, чтобы присоедениться к игре"
+        if len(self._players) < 2:  # noqa: PLR2004
+            res = "Нажмите на поле, чтобы присоединиться к игре"
         else:
             res = ""
         for i, user in enumerate(self._players):
@@ -341,39 +336,38 @@ class TicTacToeView(miru.View):
         return res
 
     def get_game_status(self) -> hikari.Embed:
-        """Возаращет сообщение со статусом игры.
+        """Возвращает сообщение со статусом игры.
 
         данное сообщение будет обновляться при каждом нажатии кнопки.
-        Возаращет название, описание, кто сейчас ходит, режим игры,
-        правила, а также спиос игроков.
+        Возвращает название, описание, кто сейчас ходит, режим игры,
+        правила, а также список игроков.
 
         Словом, вся необходимая информация о текущем ходе игры.
         Когда игра будет закончена с некоторым результатом, данное
         сообщение будет заменено другим.
 
-        :return: Сообщение с текушим статусом игры.
+        :return: Сообщение с текущим статусом игры.
         :rtype: hikari.Embed
         """
-        return hikari.Embed(
-            title=f"{_TTT_SIM[self._cur]} Крестики-нолики",
-            description=(
-                "Перед вами игровое поле.\n"
-                "Вы сошлись здесь в дуэли, чтобы доказать свою силу.\n"
-                "Мешать вам будет только ограниченное поле, да ваш противник."
-            ),
-            colour=hikari.colors.Color(0x00b0f4)
-        ).add_field(
-            name="Режим игры",
-            value="Бесконечный" if self.endless else "Обычный.",
-            inline=True
-        ).add_field(
-            name="Игроки", value=self.get_players(), inline=True
-        ).add_field(
-            name="Правила игры",
-            value=(
-            "- Нажмите на свободно поле, чтобы отметить его своим знаком.\n"
-            "- Для победы соберите 3 знака  по диагонали/вертикали/горизонтали.\n"
-            "- **Бесконечный режим**: Со временем клетки будут освобождаться."
+        return (
+            hikari.Embed(
+                title=f"{_TTT_SIM[self._cur]} Крестики-нолики",
+                description=(
+                    "Перед вами игровое поле.\n"
+                    "Вы сошлись здесь в дуэли, чтобы доказать свою силу.\n"
+                    "Мешать вам будет только ограниченное поле и ваш противник."
+                ),
+                colour=hikari.colors.Color(0x00B0F4),
+            )
+            .add_field(
+                name="Режим игры",
+                value="Бесконечный" if self.endless else "Обычный.",
+                inline=True,
+            )
+            .add_field(name="Игроки", value=self.get_players(), inline=True)
+            .add_field(
+                name="Правила игры",
+                value=_GAME_RULES,
             )
         )
 
@@ -381,7 +375,7 @@ class TicTacToeView(miru.View):
         """Сообщение при ничьей.
 
         Используется по окончанию игры в ничейную пользу.
-        Данный исход возмозможен при стандартном режиме игры, когда
+        Данный исход возможен при стандартном режиме игры, когда
         не остаётся свободных клеток.
 
         Отображает количество описание, режим игры и игроков.
@@ -389,46 +383,47 @@ class TicTacToeView(miru.View):
         :return: Сообщение с результатами игры.
         :rtype: hikari.Embed
         """
-        return hikari.Embed(
-            title=f"{_TTT_SIM[self._cur]} Крестики-нолики / Ничья",
-            description=(
-                "Это была долгая битва.\n"
-                "Однако поле закончилось.\n"
-                "Теперь не получится выяснить кто выиграл."
-            ),
-            colour=hikari.colors.Color(0xffbe6f)
-        ).add_field(
-            name="Режим игры", value="Обычный", inline=True
-        ).add_field(
-            name="Игроки", value=self.get_players(), inline=True
+        return (
+            hikari.Embed(
+                title=f"{_TTT_SIM[self._cur]} Крестики-нолики / Ничья",
+                description=(
+                    "Это была долгая битва.\n"
+                    "Однако поле закончилось.\n"
+                    "Теперь не получится выяснить кто выиграл."
+                ),
+                colour=hikari.colors.Color(0xFFBE6F),
+            )
+            .add_field(name="Режим игры", value="Обычный", inline=True)
+            .add_field(name="Игроки", value=self.get_players(), inline=True)
         )
 
     def end_game_message(self, winner: hikari.User) -> hikari.Embed:
         """Сообщение при победе одного из игроков.
 
-        Отобраажется сразу как только кто-то из игроков обержал победу.
-        Отобаражет описание, победителя, режим, список игроков.
+        Отображается сразу как только кто-то из игроков одержал победу.
+        Отображает описание, победителя, режим, список игроков.
 
         :param winner: Победитель игры.
         :typw winner: hikari.User
         :return: Сообщение с результатами игры.
         :rtype: hikari.Embed
         """
-        return hikari.Embed(
-            title=f"{_TTT_SIM[self._cur]} Крестики-нолики / Игра завершена",
-            description=(
-                "Это была долгая битва.\n"
-                "И толко один мог выйти из неё победителем.\n\n"
-                f"**Победитель**: {winner.mention}"
-            ),
-            colour=hikari.colors.Color(0x8ff0a4)
-
-        ).add_field(
-            name="Режим игры",
-            value="Бесконечный" if self.endless else "Обычный.",
-            inline=True
-        ).add_field(
-            name="Игроки", value=self.get_players(), inline=True
+        return (
+            hikari.Embed(
+                title=f"{_TTT_SIM[self._cur]} Крестики-нолики / Игра завершена",
+                description=(
+                    "Это была долгая битва.\n"
+                    "И только один мог выйти из неё победителем.\n\n"
+                    f"**Победитель**: {winner.mention}"
+                ),
+                colour=hikari.colors.Color(0x8FF0A4),
+            )
+            .add_field(
+                name="Режим игры",
+                value="Бесконечный" if self.endless else "Обычный.",
+                inline=True,
+            )
+            .add_field(name="Игроки", value=self.get_players(), inline=True)
         )
 
     def get_current_player(self) -> str:
@@ -440,7 +435,7 @@ class TicTacToeView(miru.View):
         :rtype: str
         """
         res = _TTT_SIM[self._cur]
-        if len(self._players) < 2:
+        if len(self._players) < 2:  # noqa: PLR2004
             return res + " игрока"
         else:
             return f"{res} {self._players[self._cur].mention}"
@@ -451,7 +446,7 @@ class TicTacToeView(miru.View):
         Это может быть как просто сторонний пользователь, так и игрок,
         до которого ещё не дошла очередь.
         В таком случае будет отправлено сообщение с ошибкой.
-        Данное сообщение будет удалено через некоотрое время, чтобы
+        Данное сообщение будет удалено через некоторое время, чтобы
         не засорять чат.
 
         :return: Сообщение о не валидном игроке.
@@ -465,20 +460,22 @@ class TicTacToeView(miru.View):
                 "Так или иначе сейчас ход:"
                 f"{self.get_current_player()}"
             ),
-            colour=hikari.colors.Color(0xdc8add)
+            colour=hikari.colors.Color(0xDC8ADD),
         )
 
 
 # определение команд
 # ==================
 
+
 @plugin.include
 @arc.slash_command("ttt", description="Начать игру крестики-нолики.")
 async def nya_handler(
     ctx: arc.GatewayContext,
-    endless: arc.Option[bool, arc.BoolParams("Бесконечная ли игр (нет)")
+    endless: arc.Option[
+        bool, arc.BoolParams("Бесконечная ли игр (нет)")
     ] = False,
-    client: miru.Client = arc.inject()
+    client: miru.Client = arc.inject(),
 ) -> None:
     """Начинает новую игру крестики-нолики.
 
@@ -495,10 +492,12 @@ async def nya_handler(
 # Загрузчики и выгрузчики плагина
 # ===============================
 
+
 @arc.loader
 def loader(client: arc.GatewayClient) -> None:
     """Производит загрузку плагина."""
     client.add_plugin(plugin)
+
 
 @arc.unloader
 def unloader(client: arc.GatewayClient) -> None:
