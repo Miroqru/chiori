ea0ebf0061ab5a781994952dd59852f3f2c01cbe
Milinuri Nirvalen <pentergust@mail.ru>
2025-06-15 01:39:28 +0400

    Ext:Funbox: Use plugin config to configure games answers

--- extensions/funbox.py
+++ extensions/funbox.py
@@ -7,8 +7,11 @@
 -------------
 
 - /ping: Проверить что бот работает.
+- /dice: Подбросить кубик.
+- /flip: Подбросить монетку.
+- /ball: Совет от мудрого шара.
 
-Version: v0.2 (2)
+Version: v0.3 (4)
 Author: Milinuri Nirvalen
 """
 
@@ -17,7 +20,30 @@ import random
 import arc
 import hikari
 
-plugin = arc.GatewayPlugin("funbox")
+from chioricord.config import PluginConfig, PluginConfigManager
+
+plugin = arc.GatewayPlugin("Funbox")
+
+
+class FunboxConfig(PluginConfig):
+    """Различные настройки для коробки весёлостей."""
+
+    flip_results: list[str] = ["Орла", "Решку"]
+    ball_prefix: list[str] = [
+        "Совершенно точно ",
+        "определённо ",
+        "скорее всего ",
+        "звезды подсказывают что ",
+        "возможно ",
+        "вероятно ",
+    ]
+    ball_result: list[str] = [
+        "да",
+        "нет",
+        "попробуйте позже",
+        "не знаю",
+        "не уверен",
+    ]
 
 
 # определение команд
@@ -25,51 +51,45 @@ plugin = arc.GatewayPlugin("funbox")
 
 
 @plugin.include
-@arc.slash_command("ping", description="Проверить что бот работает")
-async def ping(ctx: arc.GatewayContext):
-    """Проверить что бот в сети."""
-    await ctx.respond("Понг!")
-
-
-# Простенькие игры -------------------------------------------------------------
+@arc.slash_command("ping", description="Проверить работу бота.")
+async def ping(ctx: arc.GatewayContext) -> None:
+    """Проверить что бот в сети и отвечает на запросы."""
+    await ctx.respond("🏓 Понг!")
 
 
 @plugin.include
-@arc.slash_command("dice", description="Подбросить кубик")
-async def dice(
+@arc.slash_command("dice", description="Подбросить кубик.")
+async def roll_dice(
     ctx: arc.GatewayContext,
     sides: arc.Option[int, arc.IntParams("Сколько сторон у кубика (6)")] = 6,
     count: arc.Option[int, arc.IntParams("Сколько будет кубиков (1)")] = 1,
-):
+) -> None:
     """Подбрасывает кубик.
 
     По умолчанию подбрасывает 1 кубик с 6-тью гранями.
     Вы можете переопределить, сколько будет кубиков и сколько будет
     у них граней.
     Сообщение с результатом содержит настройки кубиков, результаты для
-    подброса каждого кубика и конечную сумму.
+    каждого кубика и конечную сумму.
     """
     # Валидация входящих параметров
     sides = min(max(3, sides), 100)
     count = min(max(1, count), 10)
-    if count > 1:
-        result = [random.randint(1, sides) for _ in range(count)]
-    else:
-        result = random.randint(1, sides)
+    result = [random.randint(1, sides) for _ in range(count)]
 
     # Собираем красивое сообщение
     emb = hikari.Embed(
         title="🎲 Подбросили кубики",
         description=(
             "Вы берёте свои любимые кубики.\n"
-            "Немного потреся их в ладонях, вы, лёгким движением руки,"
+            "Немного встряхнув их в ладонях, вы, лёгким движением руки,"
             "выбрасываете их на стол.\n\n"
-            "Чтоже тут у нас?"
+            "Что же тут у нас?"
         ),
         color=hikari.colors.Color(0xF66151),
     )
 
-    # Доабвляем рузальтат подсчётов
+    # Добавляем результат подсчётов
     if count == 1:
         emb.add_field(name="Итого", value=str(result))
     else:
@@ -79,54 +99,43 @@ async def dice(
         str_value += f" = {sum(result)}"
         emb.add_field(name="Итого", value=str_value)
 
-    return await ctx.respond(embed=emb)
-
-
-_FLIP_RESULTS = ["Орла", "Решку"]
+    await ctx.respond(emb)
 
 
 @plugin.include
-@arc.slash_command("flip", description="Подбросить монетку")
-async def flip_handler(ctx: arc.GatewayContext):
+@arc.slash_command("flip", description="Подбросить монетку.")
+async def flip_coin(
+    ctx: arc.GatewayContext, cm: PluginConfigManager = arc.inject()
+) -> None:
     """Подбрасывает монетку.
 
     Вы можете подбросить только одну монетку.
-    В рузльтате может выпаст орёл или решка.
+    В результате может выпасть орёл или решка.
     Сообщение с результатом содержит выпавшую сторону монетки.
     """
+    config: FunboxConfig = cm.get_group("funbox")
     result = random.randint(0, 1)
-    result_str = _FLIP_RESULTS[result]
-    await ctx.respond(f"🪙 Подбросив монетку вы увидели там {result_str}")
-
-
-# 8шар -------------------------------------------------------------------------
-
-_BALL_PREFIX = [
-    "соверенно точно ",
-    "определённо ",
-    "скорее всего ",
-    "звезды подчказывают что ",
-    "возможно ",
-    "вероятно ",
-]
-
-_BALL_RESULT = ["да", "нет", "попробуйте позже", "не знаю", "не уверен"]
+    result_str = config.flip_results[result]
+    await ctx.respond(f"🪙 Подбросив монетку вы увидели там {result_str}.")
 
 
 @plugin.include
-@arc.slash_command("ball", description="Получить совет от шара")
-async def flip_handler(ctx: arc.GatewayContext):
+@arc.slash_command("ball", description="Совет от мудрого шара.")
+async def flip_ball(
+    ctx: arc.GatewayContext, cm: PluginConfigManager = arc.inject()
+) -> None:
     """8 шар.
 
     Вы можете задать любой вопрос, а мудрый шар подскажет вам ответ.
     На самом деле ответ подбирается случайный.
     Но так ведь даже веселее.
     """
+    config: FunboxConfig = cm.get_group("funbox")
     if random.randint(0, 1) == 1:
-        prefix = random.choice(_BALL_PREFIX)
+        prefix = random.choice(config.ball_prefix)
     else:
         prefix = ""
-    result = random.choice(_BALL_RESULT)
+    result = random.choice(config.ball_result)
 
     await ctx.respond(f"🔮 **Мудрый шар говорит Вам**:\n\n> {prefix}{result}")
 
@@ -137,9 +146,13 @@ async def flip_handler(ctx: arc.GatewayContext):
 
 @arc.loader
 def loader(client: arc.GatewayClient) -> None:
+    """Действия при загрузке плагина."""
     client.add_plugin(plugin)
+    cm: PluginConfigManager = client.get_type_dependency(PluginConfigManager)
+    cm.set_group("funbox", FunboxConfig)
 
 
 @arc.unloader
 def unloader(client: arc.GatewayClient) -> None:
+    """Действия при выгрузке плагина."""
     client.remove_plugin(plugin)
