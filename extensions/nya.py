5c18eea8b09dcda4da559bd7f6dabacac19ae3b8
Milinuri Nirvalen <pentergust@mail.ru>
2025-06-14 10:40:53 +0400

    Ext:Nya: Update plugin formating

--- extensions/nya.py
+++ extensions/nya.py
@@ -3,25 +3,21 @@
 да, это маленькое расширение нужно чтобы някать ваших участников,
 не больше.
 
-.. tip:: Шаблонное расширение.
-
-    Вы Легко можете начать писать своё расширение для Чиори,
-    взяв за основу исходный код данного расширения.
+> [!tip] Шаблонное расширение.
+> Вы Легко можете начать писать своё расширение для Чиори,
+> взяв за основу исходный код данного расширения.
 
 Предоставляет
 -------------
 
 - /nya <member> - Някнуть участника
 
-Version: v0.4 (5)
+Version: v0.4 (6)
 Author: Milinuri Nirvalen
 """
 
 import arc
 import hikari
-
-# Глобальные переменные
-# =====================
 
 plugin = arc.GatewayPlugin("Nya")
 
@@ -29,13 +25,14 @@ plugin = arc.GatewayPlugin("Nya")
 # определение команд
 # ==================
 
+
 @plugin.include
 @arc.slash_command("nya", description="Скажи ня участнику сервера.")
 async def nya_handler(
     ctx: arc.GatewayContext,
     user: arc.Option[
         hikari.User | None, arc.UserParams("Кого нужно някнуть")
-    ] = None
+    ] = None,
 ) -> None:
     """Первая няшная команда для бота.
 
@@ -51,10 +48,12 @@ async def nya_handler(
 # Загрузчики и выгрузчики плагина
 # ===============================
 
+
 @arc.loader
 def loader(client: arc.GatewayClient) -> None:
     """Действия при загрузке плагина."""
     client.add_plugin(plugin)
+
 
 @arc.unloader
 def unloader(client: arc.GatewayClient) -> None:
