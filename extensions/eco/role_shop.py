426d25a24bda370eaab7e4edd4df327c45cfc5fb
Milinuri Nirvalen <pentergust@mail.ru>
2025-07-26 21:32:13 +0400

    Ext: New extension Roles shop

---
+++ extensions/eco/role_shop.py
@@ -0,0 +1,176 @@
+"""Магазин ролей для сервере."""
+
+import arc
+import hikari
+
+from chioricord.db import ChioDB
+from libs.coinengine import CoinsTable
+from libs.role_shop import GuildRole, RoleShopTable
+
+plugin = arc.GatewayPlugin("Roles shop")
+
+role_shop = plugin.include_slash_group("rshop", "Магазин пролей")
+
+
+async def _get_role(client: arc.GatewayClient, role: GuildRole) -> hikari.Role:
+    return client.cache.get_role(role.role) or await client.rest.fetch_role(
+        role.guild_id, role.role
+    )
+
+
+@role_shop.include
+@arc.slash_subcommand("list", description="Доступные для покупки роли.")
+async def list_shop_handler(
+    ctx: arc.GatewayContext, shop: RoleShopTable = arc.inject()
+) -> None:
+    """Все доступные для покупки роли."""
+    if ctx.guild_id is None:
+        await ctx.respond("Выполните это команду на сервере.")
+        return
+
+    roles = await shop.get_shop(ctx.guild_id)
+    res: list[str] = []
+    for shop_role in sorted(roles, key=lambda r: r.price):
+        role = await _get_role(ctx.client, shop_role)
+        res.append(f"- {role.mention}: {shop_role.price}")
+
+    emb = hikari.Embed(
+        title="Магазин ролей",
+        description="\n".join(res),
+        color=hikari.Color(0xFFCC66),
+    )
+    await ctx.respond(emb)
+
+
+@role_shop.include
+@arc.slash_subcommand("byu", description="Купить роль.")
+async def byu_shop_handler(
+    ctx: arc.GatewayContext,
+    role: arc.Option[hikari.Role, arc.RoleParams("Роль для продажи")],
+    shop: RoleShopTable = arc.inject(),
+    coins: CoinsTable = arc.inject(),
+) -> None:
+    """купить роль."""
+    if ctx.guild_id is None or ctx.member is None:
+        await ctx.respond("Выполните это команду на сервере.")
+        return
+
+    db_role = await shop.get_role(ctx.guild_id, role.id)
+    if db_role is None:
+        await ctx.respond("Роль не выставлена на продажу.")
+        return
+
+    member_roles = await ctx.member.fetch_roles()
+    if role in member_roles:
+        await ctx.respond("У вас уже приобретена данная роль.")
+        return
+
+    if db_role.require_role is not None:
+        req_role = await ctx.client.rest.fetch_role(
+            ctx.guild_id, db_role.require_role
+        )
+
+        if req_role not in member_roles:
+            await ctx.respond(
+                f"Для начала вам нужно приобрести {req_role.mention}"
+            )
+            return
+
+    res = await coins.take(ctx.user.id, db_role.price)
+    if not res:
+        await ctx.respond("Кажется у вас недостаточно средств.")
+        return
+
+    await ctx.member.add_role(role)
+
+    emb = hikari.Embed(
+        title="Успешная покупка",
+        description=f"Вы приобрели {role.mention} за {db_role.price} монеток",
+        color=hikari.Color(0xFFCC66),
+    )
+    await ctx.respond(emb)
+
+
+# Управление магазином
+# ====================
+
+
+@role_shop.include
+@arc.with_hook(arc.has_permissions(hikari.Permissions.MANAGE_ROLES))
+@arc.slash_subcommand("add_role", description="Добавить роль для продажи.")
+async def add_role_handler(
+    ctx: arc.GatewayContext,
+    role: arc.Option[hikari.Role, arc.RoleParams("Роль для продажи")],
+    price: arc.Option[int, arc.IntParams("Цена покупки")],
+    shop: RoleShopTable = arc.inject(),
+) -> None:
+    """Все доступные для покупки роли."""
+    await shop.set_price(ctx.guild_id, role.id, price)  # type: ignore
+    emb = hikari.Embed(
+        title="Добавлена роль",
+        description=f"{role.mention} за {price} монеток.",
+        color=hikari.Color(0xFFCC66),
+    )
+    await ctx.respond(emb)
+
+
+@role_shop.include
+@arc.with_hook(arc.has_permissions(hikari.Permissions.MANAGE_ROLES))
+@arc.slash_subcommand("remove_role", description="Удаляет роль из магазина.")
+async def remove_role_handler(
+    ctx: arc.GatewayContext,
+    role: arc.Option[hikari.Role, arc.RoleParams("Роль для удаления")],
+    shop: RoleShopTable = arc.inject(),
+) -> None:
+    """Все доступные для покупки роли."""
+    await shop.remove_role(ctx.guild_id, role.id)  # type: ignore
+    emb = hikari.Embed(
+        title="Удалена роль",
+        description=f"{role.mention} удалена из магазина",
+        color=hikari.Color(0xFFCC66),
+    )
+    await ctx.respond(emb)
+
+
+@role_shop.include
+@arc.with_hook(arc.has_permissions(hikari.Permissions.MANAGE_ROLES))
+@arc.slash_subcommand(
+    "set_require", description="Устанавливает необходимую роль."
+)
+async def set_require_handler(
+    ctx: arc.GatewayContext,
+    role: arc.Option[hikari.Role, arc.RoleParams("Роль для удаления")],
+    require: arc.Option[
+        hikari.Role | None, arc.RoleParams("Роль для удаления")
+    ] = None,
+    shop: RoleShopTable = arc.inject(),
+) -> None:
+    """Все доступные для покупки роли."""
+    if require is None:
+        req_id = None
+        desc = f"Для покупки {role.mention} не требуется дополнительных ролей."
+    else:
+        req_id = require.id
+        desc = f"Для покупки {role.mention} требуется купить {require.mention}"
+
+    await shop.set_require(ctx.guild_id, role.id, req_id)  # type: ignore
+    emb = hikari.Embed(
+        title="Обновлена роль",
+        description=desc,
+        color=hikari.Color(0xFFCC66),
+    )
+    await ctx.respond(emb)
+
+
+@arc.loader
+def loader(client: arc.GatewayClient) -> None:
+    """Actions on plugin load."""
+    client.add_plugin(plugin)
+    cm = client.get_type_dependency(ChioDB)
+    cm.register(RoleShopTable)
+
+
+@arc.unloader
+def unloader(client: arc.GatewayClient) -> None:
+    """Actions on plugin unload."""
+    client.remove_plugin(plugin)
---
+++ libs/role_shop.py
@@ -0,0 +1,117 @@
+"""Магазин ролей для сервера."""
+
+from dataclasses import dataclass
+from typing import Self
+
+from asyncpg import Record
+
+from chioricord.db import DBTable
+
+
+@dataclass(frozen=True, slots=True)
+class GuildRole:
+    """Роль на продажу на сервере."""
+
+    guild_id: int
+    role: int
+    price: int
+    require_role: int | None
+
+    @classmethod
+    def from_row(cls, row: Record) -> Self:
+        """Собирает GuildRole из базы данных."""
+        return cls(row[1], row[2], row[3], row[4])
+
+
+class RoleShopTable(DBTable):
+    """Таблица магазина ролей для гильдии."""
+
+    __tablename__ = "roles_shop"
+
+    async def create_table(self) -> None:
+        """Создаёт таблицы для базы данных."""
+        await self.pool.execute(
+            "CREATE TABLE IF NOT EXISTS roles_shop ("
+            "id SERIAL NOT NULL PRIMARY KEY,"
+            "guild_id BIGINT NOT NULL,"
+            "role_id BIGINT NOT NULL,"
+            "price INTEGER NOT NULL,"
+            "require_role BIGINT)"
+        )
+
+    async def get_shop(self, guild_id: int) -> list[GuildRole]:
+        """Возвращает все роли для г."""
+        cur = await self.pool.fetch(
+            f"SELECT * FROM {self.__tablename__} WHERE guild_id=$1", guild_id
+        )
+        return [GuildRole.from_row(row) for row in cur]
+
+    async def get_role(self, guild_id: int, role_id: int) -> GuildRole | None:
+        """Retrieve guild by ID from the database."""
+        cur = await self.pool.fetchrow(
+            f"SELECT * FROM {self.__tablename__} "
+            "WHERE guild_id=$1 AND role_id=$2",
+            guild_id,
+            role_id,
+        )
+        return None if cur is None else GuildRole.from_row(cur)
+
+    async def add_role(self, role: GuildRole) -> None:
+        """Create a new role record in the database."""
+        await self.pool.execute(
+            f"INSERT INTO {self.__tablename__}"
+            "(guild_id,role_id,price,require_role) VALUES($1,$2,$3,$4)",
+            role.guild_id,
+            role.role,
+            role.price,
+            role.require_role,
+        )
+
+    async def remove_role(self, guild_id: int, role_id: int) -> None:
+        """удаляет роль из магазина."""
+        await self.pool.execute(
+            f"DELETE FROM {self.__tablename__} "
+            "WHERE guild_id=$1 AND role_id=$2",
+            guild_id,
+            role_id,
+        )
+
+    async def set_require(
+        self, guild_id: int, role_id: int, required_role: int | None
+    ) -> GuildRole:
+        """Устанавливает требуемую роль перед покупкой."""
+        role = await self.get_role(guild_id, role_id)
+        if role is None:
+            new_role = GuildRole(guild_id, role_id, 1000, required_role)
+            await self.add_role(new_role)
+        else:
+            new_role = GuildRole(guild_id, role_id, role.price, required_role)
+            await self.pool.execute(
+                f"UPDATE {self.__tablename__} SET require_role=$1 "
+                "WHERE guild_id=$2 AND role_id=$3",
+                required_role,
+                guild_id,
+                role_id,
+            )
+        return new_role
+
+    async def set_price(
+        self, guild_id: int, role_id: int, price: int
+    ) -> GuildRole:
+        """Устанавливает требуемую роль перед покупкой."""
+        role = await self.get_role(guild_id, role_id)
+        if role is None:
+            new_role = GuildRole(guild_id, role_id, price, None)
+            await self.add_role(new_role)
+        else:
+            new_role = GuildRole(
+                guild_id, role_id, role.price, role.require_role
+            )
+            await self.pool.execute(
+                f"UPDATE {self.__tablename__} SET price=$1 "
+                "WHERE guild_id=$2 AND role_id=$3",
+                price,
+                guild_id,
+                role_id,
+            )
+        return new_role
