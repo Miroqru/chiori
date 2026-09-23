cc60c667ee4187862a3523bc11b7e457a66bc914
Milinuri Nirvalen <pentergust@mail.ru>
2026-09-23 11:31:09 +0400

    Chore: Update nox file; fix ruff errors

--- noxfile.py
+++ noxfile.py
@@ -1,10 +1,10 @@
-import os
 import typing as t
+from pathlib import Path
 
 import nox
 from nox import options
 
-PATH_TO_PROJECT = os.path.join(".")
+PATH_TO_PROJECT = Path()
 SCRIPT_PATHS = [PATH_TO_PROJECT, "noxfile.py"]
 
 options.sessions = ["format_fix", "pyright"]
@@ -18,6 +18,7 @@ def uv_sync(
     extras: t.Sequence[str] = (),
     groups: t.Sequence[str] = (),
 ) -> None:
+    """Синхронизирует зависимости при помощи Uv."""
     if extras and not include_self:
         raise RuntimeError("When specifying extras, set `include_self=True`.")
 
@@ -40,14 +41,16 @@ def uv_sync(
 
 
 @nox.session()
-def format_fix(session: nox.Session):
+def format_fix(session: nox.Session) -> None:
+    """Форматирует код и автоматически исправляет ошибки линтера."""
     session.install("-U", "ruff")
     session.run("python", "-m", "ruff", "format", *SCRIPT_PATHS)
     session.run("python", "-m", "ruff", "check", *SCRIPT_PATHS, "--fix")
 
 
 @nox.session()
-def format(session: nox.Session) -> None:
+def format(session: nox.Session) -> None:  # noqa: A001
+    """Форматирует код под единый стандарт."""
     session.install("-U", "ruff")
     session.run("python", "-m", "ruff", "format", *SCRIPT_PATHS, "--check")
     session.run("python", "-m", "ruff", "check", *SCRIPT_PATHS)
@@ -55,5 +58,6 @@ def format(session: nox.Session) -> None:
 
 @nox.session()
 def pyright(session: nox.Session) -> None:
+    """Проверяет код на ошибки типизации."""
     uv_sync(session, include_self=True, groups=["dev"])
     session.run("pyright", *SCRIPT_PATHS)
